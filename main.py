ef02d2fe6609797fc55c1eb967f1a28c6faa82e8
Nicolle Santizo <95056257+santizonicolle@users.noreply.github.com>
2022-11-11 20:06:14 -0600

    Update variables

---
+++ main.py
@@ -0,0 +1,8 @@
+import csv
+
+
+print("¡Bienvenido! Ingrese los datos de mascota.")
+name=input("Ingrese el nombre de la mascota:")
+age=input("Ingrese la edad de la mascota:")
+dueño=input("Ingrese el nombre del dueño:")
+DNIdueño=input("Ingrese DNI de el dueño:")
