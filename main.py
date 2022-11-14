af9c13bc6a8f368848d845cfda76b7cd3be12521
Nicolle Santizo <95056257+santizonicolle@users.noreply.github.com>
2022-11-13 20:14:10 -0600

    Add While

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import csv
 
-
-
 print("¡Bienvenido!")
 print("1. Cargar un archivo csv con datos de 5 mascotas.")
 print("2. Mostrar datos de mascotas cargadas en el sistema.")
@@ -10,23 +8,24 @@ print("4. Buscar mascota.")
 print("5. Ordenar mascotas.")
 print("6. Guardar mascotas en archivo CSV.")
 
-opcion = int(input("Ingrese la opción que desee:"))
+while True:
+    opcion = int(input("Ingrese la opción que desee:"))
 
-if opcion==1:
-    print("Se cargaron los datos de 5 mascotas")
-elif opcion==2:
-    with open("pacientes.csv", encoding = 'utf-8') as f:
-        archivo =csv.reader(f)
-        for linea in archivo:
-            print(linea)
-elif opcion==3:
-    print()
-elif opcion==4:
-    print()
-elif opcion==5:
-    print()
-elif opcion==6:
-    print()
+    if opcion==1:
+        print("Se cargaron los datos de 5 mascotas")
+    elif opcion==2:
+        with open("pacientes.csv", encoding = 'utf-8') as f:
+            archivo =csv.reader(f)
+            for linea in archivo:
+                print(linea)
+    elif opcion==3:
+        print()
+    elif opcion==4:
+        print()
+    elif opcion==5:
+        print()
+    elif opcion==6:
+        print()
 
-else:
-    print("Opción Incorrecta.")
+    else:
+        print("Opción Incorrecta.")
