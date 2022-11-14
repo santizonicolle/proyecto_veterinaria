bfd33c4b40be5babecba785358c8640ed6b55fad
Nicolle Santizo <95056257+santizonicolle@users.noreply.github.com>
2022-11-13 19:30:04 -0600

    Update opciones

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import csv
 
 with open("pacientes.csv", encoding = 'utf-8') as f:
     archivo =csv.reader(f)
-    
-    
-    
-    
-    
-    
-  
-print("¡Bienvenido! Ingrese los datos de mascota.")
-name=input("Ingrese el nombre de la mascota:")
-age=input("Ingrese la edad de la mascota:")
-dueño=input("Ingrese el nombre del dueño:")
-DNIdueño=input("Ingrese DNI de el dueño:")
+
+print("¡Bienvenido! Ingrese la opción que desee:")
+print("1. Cargar un archivo csv con datos de 5 mascotas./n")
+print("2. Mostrar datos de mascotas cargadas en el sistema.")
+print("2.")
+print("2.")
+
