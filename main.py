b6fc4071f8218358d43651de7b187ec4db28b44c
Nicolle Santizo <95056257+santizonicolle@users.noreply.github.com>
2022-11-13 22:26:27 -0600

    Add option3

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import csv
 
-
-
 print("¡Bienvenido!")
 print("1. Cargar un archivo csv con datos de 5 mascotas.")
 print("2. Mostrar datos de mascotas cargadas en el sistema.")
@@ -14,13 +12,26 @@ opcion = int(input("Ingrese la opción que desee:"))
 
 if opcion==1:
     print("Se cargaron los datos de 5 mascotas")
+
 elif opcion==2:
     with open("pacientes.csv", encoding = 'utf-8') as f:
         archivo =csv.reader(f)
         for linea in archivo:
             print(linea)
+
 elif opcion==3:
-    print()
+    name=input("Ingrese el nombre de la mascota:")
+    raza=input("Ingrese la raza:")
+    age=input("Ingrese la edad de la mascota:")
+    dueño=input("Ingrese el nombre del dueño:")
+    dni_dueño=input("Ingrese DNI de el dueño:")
+
+    add_mascota=[name,raza,age,dueño,dni_dueño]
+    with open('pacientes.csv', 'a', encoding = 'utf-8') as f:
+        archivo = csv.writer(f)
+        archivo.writerow(add_mascota)
+        f.close()
+
 elif opcion==4:
     print()
 elif opcion==5:
@@ -29,4 +40,4 @@ elif opcion==6:
     print()
 
 else:
-    print("Opción Incorrecta.")
+    print("Opción Incorrecta. Elige un número válido.")
